4d10f69e4acbe456fc164c761ab9b6944d768a42
yinhang <pigbayspy@gmail.com>
2021-06-16 22:39:43 +0800

    feat(retrofit):添加 retrofit

---
+++ app/src/main/java/io/pig/lkong/http/spec/LkongSpec.kt
@@ -0,0 +1,17 @@
+package io.pig.lkong.http.spec
+
+import io.pig.lkong.http.data.LkongForumThreadResp
+import retrofit2.Call
+import retrofit2.http.GET
+
+/**
+ * 龙空接口
+ *
+ * @author yinhang
+ * @since 2021/6/16
+ */
+interface LkongSpec {
+
+    @GET("/index.php?mod=data&sars=my/favorite")
+    fun getFavorite(): Call<LkongForumThreadResp>
+}
