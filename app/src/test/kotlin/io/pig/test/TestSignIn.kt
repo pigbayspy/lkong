22ea03155d96eda047e04ae67c598c7c9eb22625
yinhang <pigbayspy@gmail.com>
2021-07-19 23:40:33 +0800

    refactor:重构测试代码

--- app/src/test/kotlin/io/pig/test/TestSignIn.kt
+++ app/src/test/kotlin/io/pig/test/TestSignIn.kt
@@ -1,5 +1,6 @@
 package io.pig.test
 
+import io.pig.lkong.BuildConfig
 import io.pig.lkong.http.source.LkongRepository
 import kotlinx.coroutines.runBlocking
 import org.junit.Test
@@ -14,14 +15,10 @@ class TestSignIn {
 
     @Test
     fun testSignIn(): Unit = runBlocking {
-        val userEmail = "469608745@qq.com"
-        val password = "WODESHENGRI19970720"
-        val resp = LkongRepository.sign(userEmail, password)
+        val userEmail = BuildConfig.lkongAccount
+        val password = BuildConfig.lkongPassword
+        val resp = LkongRepository.signIn(userEmail, password)
         assertNotNull(resp, "返回数据不为空")
-        val respData = resp.data
-        assertNotNull(respData, "登录失败")
-        val signResp = respData.login
-        assertNotNull(signResp, "登录失败")
-        assertTrue(signResp.name.isNotEmpty(), "登录失败")
+        assertTrue(resp.name.isNotEmpty(), "登录失败")
     }
 }
