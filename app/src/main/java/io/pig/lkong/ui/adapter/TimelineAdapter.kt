7652984dcf84c2fa83cb9d0c5febbccdf7a4806b
yinhang <pigbayspy@gmail.com>
2021-07-23 08:50:32 +0800

    feat:补全 TimelineAdapter

--- app/src/main/java/io/pig/lkong/ui/adapter/TimelineAdapter.kt
+++ app/src/main/java/io/pig/lkong/ui/adapter/TimelineAdapter.kt
@@ -1,9 +1,12 @@
 package io.pig.lkong.ui.adapter
 
 import android.content.Context
+import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import io.pig.lkong.R
 import io.pig.lkong.model.TimelineModel
+import io.pig.lkong.ui.adapter.item.TimelineViewHolder
 import io.pig.widget.adapter.BaseRecycleViewAdapter
 
 /**
@@ -13,13 +16,53 @@ import io.pig.widget.adapter.BaseRecycleViewAdapter
 class TimelineAdapter(
     val context: Context,
     timelines: List<TimelineModel>
-): BaseRecycleViewAdapter<TimelineModel>(timelines) {
+) : BaseRecycleViewAdapter<TimelineModel>(timelines) {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
-        TODO("Not yet implemented")
+        val isReply = (viewType == TYPE_REPLY)
+        val view = if (isReply) {
+            LayoutInflater.from(parent.context)
+                .inflate(R.layout.item_timeline, parent, false)
+        } else {
+            LayoutInflater.from(parent.context)
+                .inflate(R.layout.item_timeline_thread, parent, false)
+        }
+        return TimelineViewHolder(view)
     }
 
     override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
         super.onBindViewHolder(holder, position)
+        val viewHolder = holder as TimelineViewHolder
+        val item = getItem(position)
+        val isReply = (getItemViewType(position) == TYPE_REPLY)
+        if (isReply) {
+            bindReplyItem(viewHolder, item)
+        } else {
+            bindThreadItem(viewHolder, item)
+        }
+    }
+
+    override fun getItemViewType(position: Int): Int {
+        val item = getItem(position)
+        return if (item.isQuote || !item.isThread) {
+            TYPE_REPLY
+        } else if (item.isThread) {
+            TYPE_THREAD
+        } else {
+            TYPE_THREAD
+        }
+    }
+
+    private fun bindThreadItem(holder: TimelineViewHolder, item: TimelineModel) {
+
+    }
+
+    private fun bindReplyItem(holder: TimelineViewHolder, item: TimelineModel) {
+
+    }
+
+    companion object {
+        const val TYPE_REPLY = 1
+        const val TYPE_THREAD = 0
     }
 }
