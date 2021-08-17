f0922698781e89a99879ea7d850f71954cc1e4cb
Yin Hang <pigbayspy@gmail.com>
2021-08-17 03:43:47 +0000

    feat: 添加查询帖子列表的请求体

---
+++ app/src/main/java/io/pig/lkong/http/data/req/ThreadPostReq.kt
@@ -0,0 +1,58 @@
+package io.pig.lkong.http.data.req
+
+class ThreadPostReq(tid:Long, page:Int) {
+    val operationName = "ViewThread"
+    val variables = mapOf<String, Any>("tid" to tid, "page" to page)
+    val query = """
+    query ViewThread(${'$'}tid: Int!, ${'$'}page:Int!) {
+        ...indexThreadComponent
+    }
+    fragment indexThreadComponent on Query {
+        thread(tid: ${'$'}tid) {
+            title
+            replies
+            views
+            status
+            lock
+            first
+            dateline
+            lastpost
+            author {
+                name
+                uid
+                avatar
+            }
+        }
+        posts(tid:${'$'}tid, page:${'$'}page) {
+            lou
+            rate {
+                dateline
+                id
+                num
+                reason
+                user {
+                    name
+                    uid
+                }
+            }
+            quote {
+                content
+                pid
+                author {
+                    name
+                    uid
+                }
+            }
+            content
+            dateline
+            status
+            editTime
+            user {
+                uid
+                name
+                avatar
+            }
+        }
+    }
+    """
+}
